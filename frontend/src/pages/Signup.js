cbdee140d091a96424da3fb9c8daa80f3f33ba71
Oren93 <orenraz117@gmail.com>
2024-05-31 17:38:43 +0000

    WIP: Few auth templates to work with

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch("http://localhost:8001/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+      console.log("Signup successful");
+    } else {
+      console.error("Signup failed");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
