3a41e8f58a687efa85a1d0a5458ffa4eab9f2e59
Oren93 <orenraz117@gmail.com>
2024-07-24 14:27:37 +0000

    Migration with retry machnism

--- backend/src/database/migrate.py
+++ backend/src/database/migrate.py
@@ -1,18 +1,20 @@
 import os
+import time
 from sqlalchemy import text, inspect
 from sqlalchemy.orm import Session
 from src.database.database import SessionLocal
 from src.database.database import engine
 from src.database.models import Base, Migration
-
+from sqlalchemy.exc import OperationalError
 from sqlalchemy import create_engine, inspect
 from src.database.database import SQLALCHEMY_DATABASE_URL
 
 MIGRATIONS_PATH = os.path.join(os.getcwd(),"src","database","migrations")
+
 def get_applied_migrations(session: Session):
     return {m.filename for m in session.query(Migration.filename).all()}
 
-def apply_migration(session: Session, filename: str):
+def apply_single_migration(session: Session, filename: str):
     with open(os.path.join(MIGRATIONS_PATH, filename), 'r') as file:
         sql_script = file.read()
 
@@ -23,15 +25,13 @@ def apply_migration(session: Session, filename: str):
         session.commit()
     except Exception as e:
         session.rollback()
-        print(f"Error applying migration {filename}: {str(e)}")
-
 
 def ensure_migrations_table(engine):
     inspector = inspect(engine)
     if not inspector.has_table("migrations"):
         Migration.__table__.create(engine)
 
-def migrate():
+def apply_migrations():
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
     ensure_migrations_table(engine)
 
@@ -42,8 +42,16 @@ def migrate():
         new_migrations = [f for f in all_migrations if f not in applied_migrations]
 
         for migration in new_migrations:
-            print(f"Applying migration: {migration}")
-            apply_migration(session, migration)
-            print(f"Migration {migration} applied successfully.")
+            apply_single_migration(session, migration)
     finally:
         session.close()
+
+def migrate(max_retries=6, retry_delay=10):
+    retries = 0
+    while retries < max_retries:
+        try:
+            apply_migrations()
+            return
+        except OperationalError as e:
+            retries += 1
+            time.sleep(retry_delay)
